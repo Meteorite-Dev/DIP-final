dbebdd6bff26f6f2f1edb5a7c2ccdd15f7d0f325
DuridMing <50489088+DuridMing@users.noreply.github.com>
2020-12-19 22:31:59 +0800

    process.py change

--- ImageProcess/process.py
+++ ImageProcess/process.py
@@ -1,7 +1,9 @@
 from Filter import EdgeDetector ,Filter
-from plot import plot3D , plot2D
+# from plot import plot3D , plot2D
+from plot import plot2D
 from IM_classical import Image_classical
 
+'''
 class ImageProcess(EdgeDetector , plot2D ,plot3D  ,Filter , Image_classical) :
     def __init__(self):
         EdgeDetector.__init__(self)
@@ -9,6 +11,12 @@ class ImageProcess(EdgeDetector , plot2D ,plot3D  ,Filter , Image_classical) :
         plot3D.__init__(self)
         Filter.__init__(self)
         Image_classical.__init__(self)
+'''
 
-
+class ImageProcess(EdgeDetector, plot2D, Filter, Image_classical):
+    def __init__(self):
+        EdgeDetector.__init__(self)
+        plot2D.__init__(self)
+        Filter.__init__(self)
+        Image_classical.__init__(self)
         
